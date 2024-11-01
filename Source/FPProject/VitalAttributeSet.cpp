1d28513034995f50cc989c02b17287288ab7e9e1
PavelBlystavets <33286097+PavelBlystavets@users.noreply.github.com>
2024-11-01 20:54:51 +0200

    Lesson 23

---
+++ Source/FPProject/VitalAttributeSet.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VitalAttributeSet.h"
+
+void UVitalAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, MaxHealth.GetCurrentValue());
+	}
+}
+
+void UVitalAttributeSet::PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue)
+{
+	Super::PostAttributeChange(Attribute, OldValue, NewValue);
+
+	if (Attribute == GetHealthAttribute())
+	{
+		if (NewValue <= 0)
+		{
+			//GetOwningAbilitySystemComponent()->AddLooseGameplayTag("Dead");
+		}
+		
+	}
+}
